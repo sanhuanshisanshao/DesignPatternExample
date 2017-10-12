6a8a285d1f9095bad4ef0e039117fb62c3b9266a
gaoq <gao19930513@outlook.com>
2017-10-12 09:31:46 +0800

    修改文件名

---
+++ composition/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+type Fly interface {
+	Fly()
+}
+
+type Quack interface {
+	Quack()
+}
+
+type Duck interface {
+	Swim()
+	Eat()
+}
+
+type BasicDuck struct {
+}
+
+func (d *BasicDuck) Swim() {
+	fmt.Println("basic duck swimming...")
+}
+
+func (d *BasicDuck) Eat() {
+	fmt.Println("basic duck eatting...")
+}
+
+type RedheadDuck struct {
+	//继承鸭子的通用接口
+	D Duck
+}
+
+func (d *RedheadDuck) Fly() {
+	fmt.Println("red head duck flying...")
+}
+
+func (d *RedheadDuck) Quack() {
+	fmt.Println("red head duck quacking...")
+}
+
+func main() {
+	var d RedheadDuck
+	d.D = &BasicDuck{}
+
+	d.D.Eat()
+	d.D.Swim()
+	d.Fly()
+	d.Quack()
+}
