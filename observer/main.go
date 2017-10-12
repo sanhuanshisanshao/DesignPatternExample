07abc48408cb35db5553399d753e8de8448ba69c
gaoq <gao19930513@outlook.com>
2017-10-12 17:08:18 +0800

    observer done

--- observer/main.go
+++ observer/main.go
@@ -65,15 +65,27 @@ func (o *CurrentObserver) Display() {
 func main() {
 	scoreSubject := &ScoreSubject{Result: "0-0", ObserverList: list.New()}
 
+	//为主题注册观察者
 	observer1 := NewCurrentObserver(scoreSubject)
 	observer2 := NewCurrentObserver(scoreSubject)
 
+	//打印观察者的当前值
 	observer1.Display()
 	observer2.Display()
+
+	//主题更新数据
 	scoreSubject.SetScoreResult("1-0")
 	observer1.Display()
 	observer2.Display()
+
 	scoreSubject.SetScoreResult("1-1")
+	observer1.Display()
+	observer2.Display()
+
+	//observer1 取消订阅主题
+	scoreSubject.removeObserver(observer1)
+	scoreSubject.SetScoreResult("2-1")
+	//observer1不再获取主题的最新推送
 	observer1.Display()
 	observer2.Display()
 }
